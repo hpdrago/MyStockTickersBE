bf6e79494354db8a7fb7cc66bb1bbcba2cd5c2c6
hpdrago <michael.earl.65@gmail.com>
2017-09-05 19:36:36 -0700

    Fixed setting of notes date.

--- src/main/java/com/stocktracker/servicelayer/service/listcopy/ListCopyStockNoteEntityToStockNoteDTO.java
+++ src/main/java/com/stocktracker/servicelayer/service/listcopy/ListCopyStockNoteEntityToStockNoteDTO.java
@@ -1,8 +1,11 @@
 package com.stocktracker.servicelayer.service.listcopy;
 
+import com.stocktracker.common.JSONDateConverter;
 import com.stocktracker.repositorylayer.entity.StockNoteEntity;
 import com.stocktracker.weblayer.dto.StockNoteDTO;
 import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
 
 /**
  * Created by mike on 9/10/2016.
@@ -13,5 +16,19 @@ public class ListCopyStockNoteEntityToStockNoteDTO extends ListCopyProperties<St
     public ListCopyStockNoteEntityToStockNoteDTO()
     {
         super( StockNoteDTO.class );
+    }
+
+    @Override
+    protected void copyProperties( final StockNoteEntity source, final StockNoteDTO target )
+    {
+        super.copyProperties( source, target );
+        try
+        {
+            target.setNotesDate( JSONDateConverter.toString( source.getNotesDate() ) );
+        }
+        catch ( ParseException e )
+        {
+            throw new IllegalArgumentException( "Failed to convert notes_date to a string", e );
+        }
     }
 }
