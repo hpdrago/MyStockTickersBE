6f7ae0e7357489d171a9d822763ed51948c7f30a
Michael Earl <michael.earl.65@gmail.com>
2018-05-22 08:41:08 -0700

    Database dump

--- data/stocktracker.dump.sql
+++ data/stocktracker.dump.sql
@@ -971,4 +971,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-20 16:01:39
+-- Dump completed on 2018-05-22  8:40:44
